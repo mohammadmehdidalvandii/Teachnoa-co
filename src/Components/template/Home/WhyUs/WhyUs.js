f18a8b752ee67af98ad63b3de756031553b31e54
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-04-08 15:57:56 +0330

    create section whyUs for home-page

---
+++ src/Components/template/Home/WhyUs/WhyUs.js
@@ -0,0 +1,157 @@
+import React from 'react'
+import style from './WhyUs.module.css';
+import { IoMdArrowBack } from "react-icons/io";
+
+
+function WhyUs() {
+  return (
+    <section className={style.whyUs}>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                    <div className={style.whyUs_content}>
+                        <h5 className={style.whyUs_content_title}>ایجاد راه حل های وب موثر</h5>
+                        <p className={style.whyUs_content_desc}>بدون زحمت در حوزه پیچیده راه حل های IT با تکنوآ مانور دهید. پلتفرم ما بهینه‌سازی‌ها را ساده می‌کند و سیستم‌های شما را به سمت محبوبیت در چشم‌انداز دیجیتال سوق می‌دهد</p>
+                        <div className={style.whyUs_content_wrapper}>
+                            <ul className={style.whyUs_content_items}>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>طراحی سایت</span>
+                                </li>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>توسعه shopify</span>
+                                </li>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>طراحی UI/UX</span>
+                                </li>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>توسعه دهنده React</span>
+                                </li>
+                            </ul>
+                            <ul className={style.whyUs_content_items}>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>توسعه دهنده وب</span>
+                                </li>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>تجارت الکترونیک</span>
+                                </li>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>توسعه دهنده وردپرس</span>
+                                </li>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>سئو</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                    <img src="/images/s1.png" alt="whyUs image" className={style.whyUs_img} />
+                </div>
+            </div>
+            <div className="row mt-5">
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                    <img src="/images/s1.png" alt="whyUs image" className={style.whyUs_img} />
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                <div className={style.whyUs_content}>
+                        <h5 className={style.whyUs_content_title}>راه حل های دیجیتال برای نیازهای فنی کسب و کار شما</h5>
+                        <p className={style.whyUs_content_desc}>بدون زحمت در حوزه پیچیده راه حل های IT با تکنوآ مانور دهید. پلتفرم ما بهینه‌سازی‌ها را ساده می‌کند و سیستم‌های شما را به سمت محبوبیت در چشم‌انداز دیجیتال سوق می‌دهد</p>
+                        <div className={style.whyUs_content_wrapper}>
+                            <ul className={style.whyUs_content_items}>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>استراتژی</span>
+                                </li>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>طراحی UI/UX</span>
+                                </li>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>برنامه آیفون</span>
+                                </li>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>بازاریابی دیجیتال</span>
+                                </li>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>دامنه و هاست</span>
+                                </li>
+                            </ul>
+                            <ul className={style.whyUs_content_items}>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>توسعه وب</span>
+                                </li>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>برنامه های اندروید</span>
+                                </li>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>خدمات ابری</span>
+                                </li>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>سئو و بهینه سازی </span>
+                                </li>
+                                <li className={style.whyUs_content_item}>
+                                    <span className={style.WhyUs_content_icon}>
+                                        <IoMdArrowBack/>
+                                    </span>
+                                    <span className={style.WhyUs_content_text}>خدمات پشتیبانی</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default WhyUs
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Navbar from '@/Components/module/Navbar/Navbar'
 import Banner from '@/Components/template/Home/Banner/Banner'
 import Services from '@/Components/template/Home/Services/Services'
+import WhyUs from '@/Components/template/Home/WhyUs/WhyUs'
 import React from 'react'
 
 function page() {
@@ -9,6 +10,7 @@ function page() {
       <Navbar/>
       <Banner/>
       <Services/>
+      <WhyUs/>
     </>
     )
 }
